7ce57588388cb572e4e73e76c72abe961a563dc8
grandauto3 <39314276+grandauto3@users.noreply.github.com>
2024-02-05 23:11:45 +0100

    add offset into the inspector

--- src/game/game.rs
+++ src/game/game.rs
@@ -27,6 +27,8 @@ pub struct State {
     map: Array2D<Tile>,
     //pub toml: Asset<String>,
     cfg: GameConfig,
+
+    pub offset: (f32, f32),
 }
 
 impl State {
@@ -38,6 +40,7 @@ impl State {
         Self {
             map: create_map(size),
             cfg,
+            offset: (20f32, 20f32),
         }
     }
 
@@ -55,7 +58,7 @@ impl State {
     pub fn set_map_size(&mut self, new: (usize, usize)) {
         self.cfg.model.map_config.set_size(new);
     }
-    pub fn redraw_map(&mut self){
+    pub fn redraw_map(&mut self) {
         self.map = create_map(self.get_map_size());
     }
 }
@@ -73,31 +76,53 @@ impl Game {
         let window_size = app.backend.window().size();
         let window_center = (window_size.0 / 2, window_size.1 / 2);
 
+        const SIZE_OF_CELL: f32 = 50f32;
+        const PADDING: f32 = 10f32;
+        const OFFSET: f32 = 10f32;
+        const CELL_WITH_PADDING: f32 = SIZE_OF_CELL + PADDING;
+
+        let pos_in_x = (map_row_len / 2) as f32;
+        let pos_in_y = (map_col_len / 2) as f32;
+
+        //we add the cell size + padding to get the "bigger cell" (cell + padding)
+        //pos_in_x/y means the middle position of the map (in a 12*12 config, 6*6 for example)
+        //we also need the OFFSET
+        //finally we subtract half of the padding because we want the center of the padding and the full padding gives us the outermost right position
+        let off_set_x = ((CELL_WITH_PADDING) * (pos_in_x)) + OFFSET - (PADDING / 2f32);
+        let off_set_y = ((CELL_WITH_PADDING) * (pos_in_y)) + OFFSET - (PADDING / 2f32);
+
         let mut index = 0;
         for x in 0..map_row_len {
             for y in 0..map_col_len {
-                const SIZE: f32 = 50f32;
-                const PADDING: f32 = 10f32;
-                const OFFSET: f32 = 10f32;
-
                 let position = (
-                    ((SIZE + PADDING) * x as f32) + OFFSET,
-                    ((SIZE + PADDING) * y as f32) + OFFSET
+                    ((CELL_WITH_PADDING) * x as f32) + OFFSET,
+                    ((CELL_WITH_PADDING) * y as f32) + OFFSET
                 );
 
-                let off_set_x = window_center.0 as usize / (map_row_len / 2) + SIZE as usize;
-                let off_set_y = window_center.1 as usize / (map_col_len / 2) + SIZE as usize;
+                let off_set_x = window_center.0 as usize / (map_row_len / 2) + SIZE_OF_CELL as usize;
+                let off_set_y = window_center.1 as usize / (map_col_len / 2) + SIZE_OF_CELL as usize;
 
-                let off_set_x = window_center.0 / 2;
-                let off_set_y = window_center.1 / 2;
+                let off_set_x = (window_center.0 / 2) - (CELL_WITH_PADDING) as u32;
+                let off_set_y = (window_center.1 / 2) - (CELL_WITH_PADDING) as u32;
+
+                let off_set_x = ((state.offset.0 as i32) + (window_center.0 / 2) as i32);
+                let off_set_y = ((state.offset.1 as i32) + (window_center.1 / 2) as i32);
+
+                let off_set_x = (state.offset.0) + OFFSET;
+                let off_set_y = (state.offset.1) + OFFSET;
 
                 //println!("offset_x: {}; offset_y: {}", off_set_x, off_set_y);
                 //println!("window_x: {}; window_y: {}", window_center.0, window_center.1);
 
                 let position = (
-                    ((SIZE + PADDING) * x as f32) + off_set_x as f32,
-                    ((SIZE + PADDING) * y as f32) + off_set_y as f32
+                    ((CELL_WITH_PADDING) * x as f32) + off_set_x,
+                    ((CELL_WITH_PADDING) * y as f32) + off_set_y,
                 );
+
+                // let position = (
+                //     ((CELL_WITH_PADDING) * x as f32) + state.offset.0,
+                //     ((CELL_WITH_PADDING) * y as f32) + state.offset.1,
+                // );
 
 
                 let color = match index % 5 {
@@ -109,11 +134,17 @@ impl Game {
                     _ => { Color::WHITE }
                 };
 
-                draw.rect(position, (SIZE, SIZE)).color(color);
+                draw.rect(position, (SIZE_OF_CELL, SIZE_OF_CELL)).color(color);
 
                 index += 1;
             }
         }
+
+        //center
+        draw.circle(5f32).position(window_center.0 as f32, window_center.1 as f32).color(Color::RED);
+
+        draw.circle(5f32).position(off_set_x as f32, off_set_y as f32);
+
         gfx.render(&draw);
 
 
--- src/game/game_ui.rs
+++ src/game/game_ui.rs
@@ -7,39 +7,64 @@ impl GameUI {
     pub fn draw_ui(state: &mut State) -> impl FnOnce(&Context) + '_ {
         |ctx| {
             TopBottomPanel::top("menu").show(ctx, |ui| {
-                if ui.button("Save config").clicked() {
-                    state.save_config();
-                }
-                if ui.button("Redraw Map").clicked() {
-                    state.redraw_map();
-                }
+                ui.horizontal(|ui| {
+                    if ui.button("Save config").clicked() {
+                        state.save_config();
+                    }
+                    if ui.button("Redraw Map").clicked() {
+                        state.redraw_map();
+                    }
+                });
             });
 
-            const SLIDER_SPEED: f64 = 0.01;
+            const SLIDER_SPEED_SIZE: f64 = 0.01;
+            const SLIDER_SPEED_POS: f64 = 0.1;
 
             Window::new("Inspector")
                 .resizable(true)
                 .show(&ctx, |ui| {
-                    ui.label("Size");
-                    ui.horizontal(|ui| {
-                        ui.label("X:");
-                        let mut x = state.get_map_size().0;
-                        let changed = ui.add(DragValue::new(&mut x).speed(SLIDER_SPEED))
-                                        .changed();
-                        state.set_map_size((x, state.get_map_size().1));
-                        if changed {
-                            state.redraw_map();
-                        }
+                    ui.scope(|ui| {
+                        ui.heading("Size");
+                        ui.horizontal(|ui| {
+                            ui.label("X:");
+                            let mut x = state.get_map_size().0;
+                            let changed = ui.add(DragValue::new(&mut x).speed(SLIDER_SPEED_SIZE))
+                                            .changed();
+                            state.set_map_size((x, state.get_map_size().1));
+                            if changed {
+                                state.redraw_map();
+                            }
+                        });
+                        ui.horizontal(|ui| {
+                            ui.label("Y:");
+                            let mut y = state.get_map_size().1;
+                            let changed = ui.add(DragValue::new(&mut y).speed(SLIDER_SPEED_SIZE))
+                                            .changed();
+                            state.set_map_size((state.get_map_size().0, y));
+                            if changed {
+                                state.redraw_map();
+                            }
+                        });
                     });
-                    ui.horizontal(|ui| {
-                        ui.label("Y:");
-                        let mut y = state.get_map_size().1;
-                        let changed = ui.add(DragValue::new(&mut y).speed(SLIDER_SPEED))
-                                        .changed();
-                        state.set_map_size((state.get_map_size().0, y));
-                        if changed {
-                            state.redraw_map();
-                        }
+                    ui.separator();
+                    ui.scope(|ui| {
+                        ui.heading("Position");
+                        ui.horizontal(|ui| {
+                            ui.label("X:");
+                            let changed = ui.add(DragValue::new(&mut state.offset.0).speed(SLIDER_SPEED_POS))
+                                            .changed();
+                            if changed {
+                                state.redraw_map();
+                            }
+                        });
+                        ui.horizontal(|ui| {
+                            ui.label("Y:");
+                            let changed = ui.add(DragValue::new(&mut state.offset.1).speed(SLIDER_SPEED_POS))
+                                            .changed();
+                            if changed {
+                                state.redraw_map();
+                            }
+                        });
                     });
                 }, );
         }
