d05c2886a151046206050da98abbdb24012c8f54
grandauto3 <39314276+grandauto3@users.noreply.github.com>
2024-10-18 22:06:19 +0200

    fix asset paths

--- src/resources/asset_handler.rs
+++ src/resources/asset_handler.rs
@@ -2,20 +2,22 @@ use enum_map::{
     enum_map,
     EnumMap,
 };
-use const_format::formatcp;
 use once_cell::unsync::Lazy;
-use crate::map::tile::tile::TileType;
+use const_format::formatcp;
+
+use crate::map::tile::tile::{TileType};
+
 
 const ASSET_PATH: &str = "./assets";
 
 pub const ASSET_PATH_MAP: Lazy<EnumMap<TileType, Option<&str>>> = Lazy::new(|| enum_map! {
 
-    TileType::EMPTY => Some(formatcp!("{ASSET_PATH}/GrasTile.png")),
-    TileType::TENT => Some(formatcp!("{ASSET_PATH}/TentTile.png")),
-    TileType::TREE=> Some(formatcp!("{ASSET_PATH}/TreeTile.png")),
+    TileType::Empty => Some(formatcp!("{ASSET_PATH}/GrasTile.png")),
+    TileType::Tent => Some(formatcp!("{ASSET_PATH}/TentTile.png")),
+    TileType::Tree=> Some(formatcp!("{ASSET_PATH}/TreeTile.png")),
     _ => None
 });
 
 pub fn get_asset_paths_vec() -> Vec<&'static str> {
-    ASSET_PATH_MAP.into_values().filter_map(|e| e).collect()
+    ASSET_PATH_MAP.into_values().flatten().collect()
 }
